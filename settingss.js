0c063c48e0c8a854530b6c39a81beea2327960ec
Nomi201 <165730192+Nomi201@users.noreply.github.com>
2025-07-18 13:52:37 +0500

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: " ",  // add your Session Id make sure it starts with malvin~
-  OWNER_NUMBER: "263714757857", // PUT OWNER NUMBER HERE
+  SESSION_ID: "levanter_180c7d7b1451b9481a8c8ab3d661ebff1f",  // add your Session Id make sure it starts with malvin~
+  OWNER_NUMBER: "923323988258", // PUT OWNER NUMBER HERE
   PREFIX: "." // prefix (e.g., ., /, !, *)
 };
